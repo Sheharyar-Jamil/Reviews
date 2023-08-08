f9789d49f4522e5adc6b884d4c728fcc85498a55
Sheharyar-Jamil <sheharyarjamil29@gmail.com>
2023-08-08 21:32:11 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Reviews from "./components/Reviews"
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Reviews />
+      </section>
+    </main>  
+  )
+}
+
+export default App
